2c197126800bd9fe7c60718f5b163dba9bc612c4
diogoditorr <35296262+diogoditorr@users.noreply.github.com>
2024-11-20 09:20:19 -0300

    feat: Guided project - Develop foreach and if-elseif-else structures to process array data in C#

--- GuidedProject/Starter/Program.cs
+++ GuidedProject/Starter/Program.cs
@@ -5,12 +5,17 @@ using System.Text.Json.Serialization;
 // initialize variables - graded assignments 
 int examAssignments = 5;
 
-string[] studentNames = new string[] { "Sophia", "Andrew", "Emma", "Logan" };
-
 int[] sophiaScores = new int[] { 90, 86, 87, 98, 100, 94, 90 };
 int[] andrewScores = new int[] { 92, 89, 81, 96, 90, 89 };
 int[] emmaScores = new int[] { 90, 85, 87, 98, 68, 89, 89, 89 };
 int[] loganScores = new int[] { 90, 95, 87, 88, 96, 96 };
+int[] beckyScores = new int[] { 92, 91, 90, 91, 92, 92, 92 };
+int[] chrisScores = new int[] { 84, 86, 88, 90, 92, 94, 96, 98 };
+int[] ericScores = new int[] { 80, 90, 100, 80, 90, 100, 80, 90 };
+int[] gregorScores = new int[] { 91, 91, 91, 91, 91, 91, 91 };
+
+// Student names
+string[] studentNames = new string[] { "Sophia", "Andrew", "Emma", "Logan", "Becky", "Chris", "Eric", "Gregor" };
 
 int[] studentScores = new int[10];
 
@@ -23,24 +28,28 @@ foreach (string name in studentNames)
     string currentStudent = name;
 
     if (currentStudent == "Sophia")
-    {
         studentScores = sophiaScores;
-    }
     else if (currentStudent == "Andrew")
-    {
         studentScores = andrewScores;
-    }
     else if (currentStudent == "Emma")
-    {
         studentScores = emmaScores;
-    }
     else if (currentStudent == "Logan")
-    {
         studentScores = loganScores;
-    }
+    else if (currentStudent == "Becky")
+        studentScores = beckyScores;
+    else if (currentStudent == "Chris")
+        studentScores = chrisScores;
+    else if (currentStudent == "Eric")
+        studentScores = ericScores;
+    else if (currentStudent == "Gregor")
+        studentScores = gregorScores;
+    else
+        continue;
 
     decimal sumAssignmentScores = 0;
-    
+
+    decimal currentStudentGrade = 0;
+
     int gradedAssignments = 0;
     foreach (int score in studentScores)
     {
@@ -55,7 +64,7 @@ foreach (string name in studentNames)
         sumAssignmentScores += score;
     }
 
-    decimal currentStudentGrade = sumAssignmentScores / examAssignments;
+    currentStudentGrade = sumAssignmentScores / examAssignments;
 
     if (currentStudentGrade >= 97)
         currentStudentLetterGrade = "A+";
