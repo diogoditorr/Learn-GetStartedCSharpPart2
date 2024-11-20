efa8f576f73de8ca5656ce174044b9adbb593874
diogoditorr <35296262+diogoditorr@users.noreply.github.com>
2024-11-20 09:20:19 -0300

    feat: Create readable code with conventions, whitespace, and comments in C#

---
+++ CreateReadableCodeConventionsWhitespaceComments/Program.cs
@@ -0,0 +1,25 @@
+﻿/* 
+    This code reverses a message, counts the number of times
+    a particular character apperas, then prints the results
+    to the console window.
+*/
+
+string message = "The quick brown fox jumps over the lazy dog.";
+
+char[] messageLetters = message.ToCharArray();
+
+Array.Reverse(messageLetters);
+
+int count = 0;
+foreach (char letter in messageLetters)
+{
+    if (letter == 'o')
+    {
+        count++;
+    }
+}
+
+string newMessage = new String(messageLetters);
+
+Console.WriteLine(newMessage);
+Console.WriteLine($"'o' appears {count} times.");
