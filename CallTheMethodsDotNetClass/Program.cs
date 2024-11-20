ea1947beb529f81a4b7d24bd88212e50da176668
diogoditorr <35296262+diogoditorr@users.noreply.github.com>
2024-11-20 09:20:19 -0300

    feat: call methods module

--- CallTheMethodsDotNetClass/Program.cs
+++ CallTheMethodsDotNetClass/Program.cs
@@ -1,2 +1,7 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿int firstValue = 500;
+int secondValue = 600;
+int largerValue;
+
+largerValue = Math.Max(firstValue, secondValue);
+
+Console.WriteLine(largerValue);
